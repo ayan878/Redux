8596eb10b811f6d3ebde04cbc99c8fed23c37cb0
ayan878 <ayanraza197@gmail.com>
2024-03-05 14:11:54 +0530

    redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,20 @@
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const reducer = (state = { counter: 0 }, action) => {
-  //! Limitation for react reducer function
-  //! 1. Synchronous function
-  //! 2. We should not mutate the original state
-
-  switch (action.type) {
-    case "INC":
-      return { counter: state.counter + 1 };
-    case "DEC":
-      return { counter: state.counter - 1 };
-    case "ADD":
-      return { counter: state.counter + action.payload };
-    default:
-      return state;
-  }
-}
-const store = createStore(reducer);
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { counter: 0 },
+  reducers: {
+    increment(state, action) {
+      state.counter += 1;
+    },
+    decrement(state, action) {
+      state.counter -= 1;
+    },
+    addBy(state, action) {
+      state.counter += action.payload;
+    },
+  },
+});
+export const actions = counterSlice.actions;
+const store = configureStore({ reducer: counterSlice.reducer });
 export default store;
